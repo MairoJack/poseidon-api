6e67189ac24fd52f0d422779286fb117586fabfb
Administrator <admin@example.com>
2021-07-23 17:27:30 +0800

    update

--- src/main/java/com/mario/poseidon/bean/search/LectureSearch.java
+++ src/main/java/com/mario/poseidon/bean/search/LectureSearch.java
@@ -1,9 +1,11 @@
 package com.mario.poseidon.bean.search;
 
+import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.mario.poseidon.bean.entity.Lecture;
+import com.mario.poseidon.bean.enums.SiteType;
 import com.mario.poseidon.common.BaseSearch;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,12 +18,14 @@ import lombok.Setter;
 public class LectureSearch implements BaseSearch<Lecture> {
 
     private String name;
+    private SiteType site;
 
     @Override
     public Wrapper<Lecture> query() {
         return Wrappers
                 .<Lecture>lambdaQuery()
                 .like(StrUtil.isNotBlank(name), Lecture::getName, name)
+                .eq(ObjectUtil.isNotNull(site), Lecture::getSite, site)
                 .orderByDesc(Lecture::getId)
                 ;
     }
